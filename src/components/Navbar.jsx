c3616e8670d856703440f616867ce6367e7eb50e
pmvaldez <valdezp69@gmail.com>
2020-11-26 17:28:11 -0300

    mobile pantalla

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,10 +14,18 @@ const Navbar = (props) => {
     return (
         <nav class="navbar navbar-dark sticky-top flex-md-nowrap p-0 shadow" style={{background:'#0076CD'}}>
             <img src={logo} alt="" class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#"/>
-            <div className="input">
+{/*             <div className="input">
             <input class="form-control dropdown-toggle" type="text" placeholder="Search" aria-label="Search"></input>
-            </div>
-            <button onClick={logOut}>Cerrar Sesion</button>
+            </div> */}
+              <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"></input>
+  <ul class="navbar-nav px-3">
+    <li class="nav-item text-nowrap">
+            <button class="nav-link" onClick={logOut}>Cerrar Sesion</button>
+            </li>
+  </ul>
         </nav>
     )
 }
