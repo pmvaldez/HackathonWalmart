c71bac0811446a282a0196e0595f79419a8ba76a
fer <amontecinoscontreras@gmail.com>
2020-11-25 22:16:12 -0300

    aceptar cambios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  const [email, setEmail] = React.useState('')
+  const [pass, setPass] = React.useState('')
+  const [error, setError] = React.useState(null)
+
+  const procesarDatos = e => {
+    e.preventDefault()
+    if (!email.trim() || !pass.trim()) {
+      console.log('Datos vacíos email!')
+      setError('Datos vacíos email!')
+      return
+    }
+    if (!pass.trim()) {
+      console.log('Datos vacíos pass!')
+      setError('Datos vacíos pass!')
+      return
+    }
+    if (pass.length < 6) {
+      console.log('6 o más carácteres')
+      setError('6 o más carácteres en pass')
+      return
+    }
+    console.log('correcto...')
+    setError(null)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1> auth</h1>
+      <div className="mt-5">
+        <h3 className="text-center">Acceso o Registro de usuarios</h3>
+        <hr />
+        <div className="row justify-content-center">
+          <div className="col-12 col-sm-8 col-md-6 col-xl-4">
+            <form>
+              <input
+                type="email"
+                className="form-control mb-2"
+                placeholder="Ingrese Email"
+              />
+              <input
+                type="password"
+                className="form-control mb-2"
+                placeholder="Ingrese Contraseña"
+              />
+              <button
+                className="btn btn-lg btn-dark btn-block"
+                type="submit"
+              >
+                Ingresar
+                        </button>
+              <button
+                className="btn btn-sm btn-info btn-block"
+                type="button"
+              >
+                ¿No tienes cuenta?
+                        </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/bd/firebase.js
@@ -0,0 +1,23 @@
+import app from 'firebase'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB2YExdSqgoW2BwaH5mawnSVl343YJSrOk",
+    authDomain: "hackaton-walmart.firebaseapp.com",
+    databaseURL: "https://hackaton-walmart.firebaseio.com",
+    projectId: "hackaton-walmart",
+    storageBucket: "hackaton-walmart.appspot.com",
+    messagingSenderId: "154120837135",
+    appId: "1:154120837135:web:518608766f4a9aff0d6b7a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = app.firestore()
+const auth = app.auth()
+
+export {
+    db,
+    auth
+}
