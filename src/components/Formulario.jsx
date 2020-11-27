ec0edcd2955cfaa6b36f0240b65ec87ff37a77d0
fer <amontecinoscontreras@gmail.com>
2020-11-27 01:25:27 -0300

    formulario conectado con firebase

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,56 +1,116 @@
 import React from 'react'
 import "../style/formulario.css"
+import { db } from '../db/firebase'
+
+
+
 const Formulario = () => {
+
+
+    const [tarea, setTarea] = React.useState('')
+    const [tareas, setTareas] = React.useState('')
+    const [fecha, setFecha] = React.useState("")
+    const [texto, setTexto] = React.useState("")
+    const [region, setRegion] = React.useState("")
+    const [concepto, setConcepto] = React.useState("")
+    const [clasificacion, setClasificacion] = React.useState("")
+
+    const agregar = async (e) => {
+        e.preventDefault()
+        if (!tarea.trim()) {
+            console.log('sin texto')
+            return
+        }
+        console.log(tarea)
+        try {
+
+            const nuevaTarea = {
+                name: tarea,
+                fecha: Date.now(),
+                fechaDos: fecha,
+                texto: texto,
+                region: region,
+                concepto: concepto,
+                clasificacion: clasificacion
+            }
+            const data = await db.collection('tareas').add({
+                name: tarea,
+                fecha: Date.now(),
+                fechaDos: fecha,
+                texto: texto,
+                region: region,
+                concepto: concepto,
+                clasificacion: clasificacion
+            })
+            setTareas([
+                ...tareas,
+                { id: data.id, ...nuevaTarea }
+            ])
+            setTarea('')
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+    console.log(region)
     return (
         <div class="contenedorAbuelo">
             <div class="contenedorFormulario">
-                <form >
+                <form onSubmit={agregar}>
                     <div class="form-row">
                         <div class="form-group col-md-5">
-                            <label for="inputEmail4">Nombre Alerta</label>
-                            <input type="text" class="form-control" id="inputNombre" placeholder="Nombre" />
+                            <label for="inputNombre">Nombre Alerta</label>
+                            <input type="text" class="form-control" id="inputNombre" placeholder="Nombre" value={tarea}
+                                onChange={e => setTarea(e.target.value)} />
                         </div>
                         <div class="form-group col-md-5">
                             <label for="example-datetime-local-input" >Fecha ingreso</label>
                             <div class="col-12">
-                                <input class="form-control" type="datetime-local" value="20-10-2020 09:00" id="example-datetime-local-input" />
+                                <input class="form-control" type="datetime" placeholder="dd-mm-aa" value={fecha}
+                                    onChange={e => setFecha(e.target.value)} />
                             </div>
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-4">
                             <label for="inputState">Region</label>
-                            <select id="inputState" class="form-control">
-                                <option selected>Todo el territorio</option>
-                                <option selected>R. Los Lagos</option>
-                                <option selected>R.Metropolitana</option>
-                                <option selected>R. Bio-Bio</option>
+                            <select id="inputState" class="form-control" onChange={e => setRegion(e.target.value)}>
+                                <option value="Todo el terriotrio"
+                                >Todo el territorio</option>
+                                <option value="Region de Los Lagos"
+                                >R. Los Lagos</option>
+                                <option value="Region Metropolitana"
+                                >R.Metropolitana</option>
+                                <option value="Region del Bio-Bio"
+                                >R. Bio-Bio</option>
                             </select>
                         </div>
                         <div class="form-group col-md-4">
                             <label for="inputState">Clasificacion</label>
-                            <select id="inputState" class="form-control">
-                                <option selected>General</option>
-                                <option selected>Salud</option>
-                                <option selected>Alimentos</option>
+                            <select id="inputState" class="form-control" onChange={e => setClasificacion(e.target.value)}>
+                                <option value="General">General</option>
+                                <option value="Salud">Salud</option>
+                                <option value="Alimentos">Alimentos</option>
                             </select>
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="inputAddress2">Conceptos</label>
-                        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+                        <input type="text" class="form-control" id="inputAddress2" placeholder="decreto,mascarilla,informe..." value={concepto}
+                            onChange={e => setConcepto(e.target.value)} />
                     </div>
 
 
                     <div class="form-group">
                         <label for="exampleFormControlTextarea1">Mensaje</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" value={texto}
+                            onChange={e => setTexto(e.target.value)}></textarea>
                     </div>
                     <div class="form-group col-md-5">
                         <label for="inputEmail4">Destinatarios</label>
                         <input type="text" class="form-control" id="inputNombre" placeholder="Nombre" />
                     </div>
-                    <button type="submit" class="btn btn-warning">Generar nueva alerta</button>
+                    <button type="submit" class="btn btn-warning" onClick={agregar}>Generar nueva alerta</button>
                 </form>
             </div>
         </div>
